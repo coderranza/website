e34d84fa9ea6d09f5c685c0b05ec221eef106f1d
coderranza <bryancarranza.works@gmail.com>
2023-05-29 02:09:13 -0600

    Professiona Experience section added. Need update.

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,179 +1,46 @@
-(function($) {
+// Obtenemos todos los elementos de encabezado de toggle
+const toggleHeaders = document.querySelectorAll('.toggle-header');
 
-	var	$window = $(window),
-		$body = $('body');
+// Iteramos sobre los encabezados y agregamos un listener de clic a cada uno
+toggleHeaders.forEach((header) => {
+  header.addEventListener('click', toggleExperience);
+});
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
-		});
+// Función para alternar la visibilidad del contenido del toggle
+function toggleExperience() {
+  // Obtenemos el elemento de contenido del toggle correspondiente al encabezado
+  const content = this.nextElementSibling;
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+  // Obtenemos el icono del encabezado
+  const icon = this.querySelector('.material-icons');
 
-	// Touch?
-		if (browser.mobile)
-			$body.addClass('is-touch');
+  // Verificamos si el contenido está activo o no
+  const isActive = content.classList.contains('active');
 
-	// Forms.
-		var $form = $('form');
+  // Contraemos cualquier otro contenido expandido
+  const expandedContent = document.querySelector('.toggle-content.active');
+  if (expandedContent && expandedContent !== content) {
+    expandedContent.style.maxHeight = '0';
+    expandedContent.classList.remove('active');
+    const expandedIcon = expandedContent.previousElementSibling.querySelector('.material-icons');
+    if (expandedIcon) {
+      expandedIcon.textContent = 'add';
+    }
+  }
 
-		// Auto-resizing textareas.
-			$form.find('textarea').each(function() {
+  // Alternamos la clase "active" para mostrar u ocultar el contenido
+  content.classList.toggle('active');
 
-				var $this = $(this),
-					$wrapper = $('<div class="textarea-wrapper"></div>'),
-					$submits = $this.find('input[type="submit"]');
-
-				$this
-					.wrap($wrapper)
-					.attr('rows', 1)
-					.css('overflow', 'hidden')
-					.css('resize', 'none')
-					.on('keydown', function(event) {
-
-						if (event.keyCode == 13
-						&&	event.ctrlKey) {
-
-							event.preventDefault();
-							event.stopPropagation();
-
-							$(this).blur();
-
-						}
-
-					})
-					.on('blur focus', function() {
-						$this.val($.trim($this.val()));
-					})
-					.on('input blur focus --init', function() {
-
-						$wrapper
-							.css('height', $this.height());
-
-						$this
-							.css('height', 'auto')
-							.css('height', $this.prop('scrollHeight') + 'px');
-
-					})
-					.on('keyup', function(event) {
-
-						if (event.keyCode == 9)
-							$this
-								.select();
-
-					})
-					.triggerHandler('--init');
-
-				// Fix.
-					if (browser.name == 'ie'
-					||	browser.mobile)
-						$this
-							.css('max-height', '10em')
-							.css('overflow-y', 'auto');
-
-			});
-
-	// Menu.
-		var $menu = $('#menu');
-
-		$menu.wrapInner('<div class="inner"></div>');
-
-		$menu._locked = false;
-
-		$menu._lock = function() {
-
-			if ($menu._locked)
-				return false;
-
-			$menu._locked = true;
-
-			window.setTimeout(function() {
-				$menu._locked = false;
-			}, 350);
-
-			return true;
-
-		};
-
-		$menu._show = function() {
-
-			if ($menu._lock())
-				$body.addClass('is-menu-visible');
-
-		};
-
-		$menu._hide = function() {
-
-			if ($menu._lock())
-				$body.removeClass('is-menu-visible');
-
-		};
-
-		$menu._toggle = function() {
-
-			if ($menu._lock())
-				$body.toggleClass('is-menu-visible');
-
-		};
-
-		$menu
-			.appendTo($body)
-			.on('click', function(event) {
-				event.stopPropagation();
-			})
-			.on('click', 'a', function(event) {
-
-				var href = $(this).attr('href');
-
-				event.preventDefault();
-				event.stopPropagation();
-
-				// Hide.
-					$menu._hide();
-
-				// Redirect.
-					if (href == '#menu')
-						return;
-
-					window.setTimeout(function() {
-						window.location.href = href;
-					}, 350);
-
-			})
-			.append('<a class="close" href="#menu">Close</a>');
-
-		$body
-			.on('click', 'a[href="#menu"]', function(event) {
-
-				event.stopPropagation();
-				event.preventDefault();
-
-				// Toggle.
-					$menu._toggle();
-
-			})
-			.on('click', function(event) {
-
-				// Hide.
-					$menu._hide();
-
-			})
-			.on('keydown', function(event) {
-
-				// Hide on escape.
-					if (event.keyCode == 27)
-						$menu._hide();
-
-			});
-
-})(jQuery);
+  // Verificamos si el contenido está activo o no
+  if (content.classList.contains('active')) {
+    // Establecemos la altura máxima del contenido para que se expanda completamente
+    content.style.maxHeight = content.scrollHeight + 'px';
+    // Cambiamos el icono a "-"
+    icon.textContent = 'remove';
+  } else {
+    // Establecemos una altura máxima específica al contraer el contenido
+    content.style.maxHeight = '0';
+    // Cambiamos el icono a "+"
+    icon.textContent = 'add';
+  }
+}
